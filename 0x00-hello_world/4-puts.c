6be02e4940316011f214557672286442f3e08834
ahmadrammah <ahmadrammah21@gmail.com>
2023-08-09 23:13:00 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - 4-puts.c
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/**
-	 * return - return 0 means its working
-	 */
 	return (0);
 }
