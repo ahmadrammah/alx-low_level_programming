3d9f33c9a63f7f9956ed400a04593f7af56f482e
ahmadrammah <ahmadrammah21@gmail.com>
2023-08-09 23:07:37 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- *4-puts.c
+ * main - 4-puts.c
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * return - return 0 means its working
+	 */
 	return (0);
 }
