21e7dda86629af45d85d498ee064abe52b81a870
ahmadrammah <ahmadrammah21@gmail.com>
2023-08-11 03:17:26 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
