3d1788411d7027c7b99e8b7f65d866818ed74c3f
ahmadrammah <ahmadrammah21@gmail.com>
2023-08-11 02:57:23 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 26)
+	{
+		putchar('a' + i);
+		i++;
+	}
+	i = 0;
+	while (i < 26)
+	{
+		putchar('A' + i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
