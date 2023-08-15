1c232a1eb9e1ee54e72d971265a7620ad4d28b7f
ahmadrammah <ahmadrammah21@gmail.com>
2023-08-15 06:36:19 -0700

    task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Adds all the natural numbers below 1024 that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum = sum + i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
