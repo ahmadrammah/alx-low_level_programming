3410ebb4053efbdd5f88f5f1e88f581d987cb47a
ahmadrammah <ahmadrammah21@gmail.com>
2023-08-15 05:39:41 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,37 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i != 98)
+			if (i <= -100)
+			{
+				_putchar('-');
+				_putchar(((-i) / 100) + '0');
+				_putchar((((-i) / 10) % 10) + '0');
+				_putchar(((-i) % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i <= -10)
+			{
+				_putchar('-');
+				_putchar(((-i) / 10) + '0');
+				_putchar(((-i) % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i < 0)
+			{
+				_putchar('-');
+				_putchar(((-i) % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i == 0)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((i != 98) && (i > 0))
 			{
 				_putchar ((i / 10) + '0');
 				_putchar((i % 10) + '0');
