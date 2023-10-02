c144a150905cf8cabc1f30cc188d2ca02790fdfe
ahmadrammah <ahmadramm21@gmail.com>
2023-10-02 03:04:19 -0700

    update2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,28 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-void close_file(int fd);
+
 char *create_buffer(char *file);
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int x;
-
-	x = close(fd);
-
-	if (cx == -1)
-	{
-		dprintf(STDERR_FILENO, "Error! %d\n", fd);
-		exit(100);
-	}
-}
+void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * @file: The name of the file buffer is storing chars for
  * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
@@ -34,11 +19,28 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error! %s\n", file);
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
 	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int x;
+
+	x = close(fd);
+
+	if (x == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
@@ -67,8 +69,7 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	while (r > 0)
-	{
+	do {
 		if (r == -1 || from == -1)
 		{
 			dprintf(STDERR_FILENO,
@@ -89,7 +90,7 @@ int main(int argc, char *argv[])
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	}
+	} while (r > 0);
 
 	free(buffer);
 	close_file(from);
